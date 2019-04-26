9c62e525bb73eb815a03f9a369c1e3a0eed2a7a0
betcherkm <betcherkm@gmail.com>
2019-04-25 21:35:02 -0700

    Creating database

---
+++ TwitchDBWebsite/Migrations/20190426042837_Initial.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TwitchDBWebsite.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Person",
+                columns: table => new
+                {
+                    PersonID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    FirstName = table.Column<string>(nullable: false),
+                    LastName = table.Column<string>(nullable: true),
+                    TwitchUN = table.Column<string>(nullable: false),
+                    LeagueUN = table.Column<string>(nullable: true),
+                    DiscordUN = table.Column<string>(nullable: true),
+                    InstagramUN = table.Column<string>(nullable: true),
+                    TwitterUN = table.Column<string>(nullable: true),
+                    isSubscribed = table.Column<bool>(nullable: false),
+                    Tier = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Person", x => x.PersonID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Person");
+        }
+    }
+}
