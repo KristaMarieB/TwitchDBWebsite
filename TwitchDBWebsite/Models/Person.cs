9201a3f08cd1c0643a6bea1b0677b4256da3f71e
betcherkm <betcherkm@gmail.com>
2019-04-25 21:42:46 -0700

    Got the database working, changed the way things display

--- TwitchDBWebsite/Models/Person.cs
+++ TwitchDBWebsite/Models/Person.cs
@@ -13,23 +13,32 @@ namespace TwitchDBWebsite.Models
         public int PersonID { get; set; }
 
         [Required]
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
 
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
 
         [Required]
+        [Display(Name = "Twitch Username")]
         public string TwitchUN { get; set; }
 
+        [Display(Name = "League of Legends Username")]
         public string LeagueUN { get; set; }
 
+        [Display(Name = "Discord Username")]
         public string DiscordUN { get; set; }
 
+        [Display(Name = "Instagram Username")]
         public string InstagramUN { get; set; }
 
+        [Display(Name = "Twitter Username")]
         public string TwitterUN { get; set; }
-        
+
+        [Display(Name = "Subscriber?")]
         public bool isSubscribed { get; set; }
 
+        [Display(Name = "Which tier?")]
         public string Tier { get; set; }
     }
 }
